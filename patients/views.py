747ac7352e8b78c46c4c1ba5f6f8efa5c0922b7c
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-23 13:57:13 -0500

    Add serializers and views for appointments, medical notes, doctors, and departments

--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from .models import Patient, Insurance, MedicalRecord
-from .serializers import PatientSerializer, InsuranceSerializer, MedicalRecordSerializer
+from .models import Patient
+from .serializers import PatientSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -14,7 +13,7 @@ from rest_framework.generics import (
 # Create your views here.
 
 
-class ListPatientView(ListAPIView):
+class ListPatientView(ListAPIView, CreateAPIView):
     allowed_methods = ["GET", "POST"]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
@@ -24,33 +23,3 @@ class DetailPatientView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ["GET", "PUT", "DELETE"]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
-
-
-class DetailPatientView(APIView):
-    allowed_methods = ["GET", "PUT", "DELETE"]
-
-    def get(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-            serializer = PatientSerializer(patient)
-            return Response(serializer.data)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-            serializer = PatientSerializer(patient, data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(pk=pk)
-            patient.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
