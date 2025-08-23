45fa63a5ebe237bda55d0da8bdf99632bc29efa7
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-23 14:10:02 -0500

    Add views and URL routing for insurances and medical records

--- patients/views.py
+++ patients/views.py
@@ -1,25 +1,44 @@
-from .models import Patient
-from .serializers import PatientSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
 
-# Create your views here.
+from .serializers import PatientSerializer, InsuranceSerializer, MedicalRecordSerializer
+from .models import Patient, Insurance, MedicalRecord
 
 
 class ListPatientView(ListAPIView, CreateAPIView):
     allowed_methods = ["GET", "POST"]
-    queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
 
 
 class DetailPatientView(RetrieveUpdateDestroyAPIView):
     allowed_methods = ["GET", "PUT", "DELETE"]
-    queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
+
+
+class ListInsuranceView(ListAPIView, CreateAPIView):
+    allowed_methods = ["GET", "POST"]
+    serializer_class = InsuranceSerializer
+    queryset = Insurance.objects.all()
+
+
+class DetailInsuranceView(RetrieveUpdateDestroyAPIView):
+    allowed_methods = ["GET", "PUT", "DELETE"]
+    serializer_class = InsuranceSerializer
+    queryset = Insurance.objects.all()
+
+
+class ListMedicalRecordView(ListAPIView, CreateAPIView):
+    allowed_methods = ["GET", "POST"]
+    serializer_class = MedicalRecordSerializer
+    queryset = MedicalRecord.objects.all()
+
+
+class DetailMedicalRecordView(RetrieveUpdateDestroyAPIView):
+    allowed_methods = ["GET", "PUT", "DELETE"]
+    serializer_class = MedicalRecordSerializer
+    queryset = MedicalRecord.objects.all()
