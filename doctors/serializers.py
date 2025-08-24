2751180809a2662543efb31724a499e7b3f67aa7
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-23 21:37:43 -0500

    Refactor Doctor and Patient serializers to specify fields and add validation for contact number during vacation

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,12 +1,36 @@
+from attr import attrs
 from rest_framework import serializers
+
+from bookings.serializers import AppointmentSerializer
 
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    appointments = AppointmentSerializer(
+        many=True, read_only=True
+    )  # Citas del doctor (relación con citas)
+
     class Meta:
         model = Doctor
-        fields = "__all__"
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "qualification",
+            "contact_number",
+            "email",
+            "address",
+            "biography",
+            "appointments",
+        ]
+
+        def validate(self, attrs):
+            if len(attrs["contact_number"]) > 10 and attrs["is_on_vacation"] == True:
+                raise serializers.ValidationError(
+                    "El número de contacto no puede tener más de 10 dígitos si el doctor está en vacaciones"
+                )
+            return super().validate(attrs)
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
