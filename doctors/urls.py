f5a9fc12623ce668aef0b65fbdebecf7c02ec934
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-23 19:50:15 -0500

    Add DoctorViewSet and update URL routing for doctors

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from doctors.viewsets import DoctorViewSet
 
 from .views import (
     ListDoctorView,
@@ -11,13 +13,14 @@ from .views import (
     DetailMedicalNoteView,
 )
 
+router = DefaultRouter()
+router.register("doctors", DoctorViewSet)
+
 urlpatterns = [
-    path("doctors/", ListDoctorView.as_view()),
-    path("doctors/<int:id>/", DetailDoctorView.as_view()),
     path("departments/", ListDepartmentView.as_view()),
     path("departments/<int:id>/", DetailDepartmentView.as_view()),
     path("doctoravailabilities/", ListDoctorAvailabilityView.as_view()),
     path("doctoravailabilities/<int:id>/", DetailDoctorAvailabilityView.as_view()),
     path("medicalnotes/", ListMedicalNoteView.as_view()),
     path("medicalnotes/<int:id>/", DetailMedicalNoteView.as_view()),
-]
+] + router.urls
